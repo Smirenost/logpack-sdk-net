b6e554a24deea7949b6c5ec93cfc42068dc0edb3
Stephan Arenswald <stephan@sarensw.com>
2020-07-14 04:41:22 +0200

    [Fix] Fix issue that stripe customer are created twice

---
+++ Helpers/RandomStringGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace FeatureNinjas.LogPack.Utilities.Helpers
+{
+    public class RandomStringGenerator
+    {
+        private static readonly Random Random = new Random();
+
+        public static string RandomString(int length)
+        {
+            const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[Random.Next(s.Length)]).ToArray());
+        }
+
+        public static string RandomNumberString(int length)
+        {
+            const string chars = "0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[Random.Next(s.Length)]).ToArray());
+        }
+    }
+}
